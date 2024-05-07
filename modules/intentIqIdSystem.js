26aad5fae189fa0e637b0e11ff95005f8135503d
Eyvaz Ahmadzada <eyvaz.ahmedzade.12@gmail.com>
2024-05-07 12:10:55 +0100

    add browser blacklist check

--- modules/intentIqIdSystem.js
+++ modules/intentIqIdSystem.js
@@ -148,7 +148,78 @@ function handleGPPData(data = {}) {
       return JSON.stringify(obj);
   }
   return JSON.stringify(data);
-};
+}
+
+/**
+ * Detects the browser using either userAgent or userAgentData
+ * @return {string} The name of the detected browser or 'unknown' if unable to detect
+ */
+function detectBrowser() {
+  try {
+      if (navigator.userAgent) {
+          return detectBrowserFromUserAgent(navigator.userAgent);
+      } else if (navigator.userAgentData) {
+          return detectBrowserFromUserAgentData(navigator.userAgentData);
+      }
+  } catch (error) {
+      console.error('Error detecting browser:', error);
+  }
+  return 'unknown';
+}
+
+/**
+* Detects the browser from the user agent string
+* @param {string} userAgent - The user agent string from the browser
+* @return {string} The name of the detected browser or 'unknown' if unable to detect
+*/
+function detectBrowserFromUserAgent(userAgent) {
+  const browserRegexPatterns = {
+      opera: /Opera|OPR/,
+      edge: /Edg/,
+      chrome: /Chrome|CriOS/,
+      safari: /Safari/,
+      firefox: /Firefox/,
+      ie: /MSIE|Trident/,
+  };
+
+  // Check for Chrome first to avoid confusion with Safari
+  if (browserRegexPatterns.chrome.test(userAgent)) {
+      return 'chrome';
+  }
+
+  // Now we can safely check for Safari
+  if (browserRegexPatterns.safari.test(userAgent) && !browserRegexPatterns.chrome.test(userAgent)) {
+      return 'safari';
+  }
+
+  // Check other browsers
+  for (const browser in browserRegexPatterns) {
+      if (browserRegexPatterns[browser].test(userAgent)) {
+          return browser;
+      }
+  }
+
+  return 'unknown';
+}
+
+/**
+* Detects the browser from the NavigatorUAData object
+* @param {NavigatorUAData} userAgentData - The user agent data object from the browser
+* @return {string} The name of the detected browser or 'unknown' if unable to detect
+*/
+function detectBrowserFromUserAgentData(userAgentData) {
+  const brandNames = userAgentData.brands.map(brand => brand.brand);
+
+  if (brandNames.includes('Microsoft Edge')) {
+      return 'edge';
+  } else if (brandNames.includes('Opera')) {
+      return 'opera';
+  } else if (brandNames.some(brand => brand === 'Chromium' || brand === 'Google Chrome')) {
+      return 'chrome';
+  }
+
+  return 'unknown';
+}
 
 /** @type {Submodule} */
 export const intentIqIdSubmodule = {
@@ -179,6 +250,17 @@ export const intentIqIdSubmodule = {
       return;
     }
 
+    const currentBrowserLowerCase = detectBrowser();
+    const browserBlackList = typeof configParams.browserBlackList === 'string' ? configParams.browserBlackList.toLowerCase(): '';
+
+    // Check if current browser is in blacklist
+    if (browserBlackList && browserBlackList.includes(currentBrowserLowerCase)) {
+      logError('User ID - intentIqId submodule: browser is in blacklist!');
+      return;
+    }
+
+    const cookieStorageEnabled = typeof configParams.enableCookieStorage === 'boolean' ? configParams.enableCookieStorage : false;
+
     // Get consent information
     const cmpData = {};
     const uspData = uspDataHandler.getConsentData();
@@ -205,7 +287,6 @@ export const intentIqIdSubmodule = {
       cmpData.gpp_sid = gppData.applicableSections;
     }
 
-    const cookieStorageEnabled = typeof configParams.enableCookieStorage === 'boolean' ? configParams.enableCookieStorage : false;
     let rrttStrtTime = 0;
     let partnerData = {};
     
